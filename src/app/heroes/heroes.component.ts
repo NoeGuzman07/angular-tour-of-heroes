53d6328e28aded5e224182fc8373740572f369d1
NoeGuzman7 <noe.guzman@uabc.edu.mx>
2022-03-02 14:59:28 -0800

    Se completo la Parte 4 del Tour de Heroes de Angular AGREGAR SERVICIOS 02/03/2022 03:00pm

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroe: Hero = {
-    id: 1,
-    name: 'Noe Norberto Guzman Lopez (1147619)'
-  };
-  constructor() { }
 
-  ngOnInit(): void {
+  heroes: Hero[] | undefined;
+  selectedHero: Hero | undefined;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Se seleccionno el Heroes con el id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
