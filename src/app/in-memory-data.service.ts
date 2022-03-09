d22e5a6e64af528b64664c7afab8d8c6155e90e6
NoeGuzman7 <noe.guzman@uabc.edu.mx>
2022-03-08 20:36:38 -0800

    Se completo la Parte 6 del Tour de Heroes de Angular OBTENER DATOS DE UN SERVIDOR 08/03/2022 08:36pm

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
+    return {heroes};
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // (Se anula el metodo getId que garantiza que un Heores siempre tenga un ID)
+
+  // If the heroes array is empty,
+  //(Si los Heroes siempre esta Vacio)
+
+  // the method below returns the initial number (11).
+  //(El Metodo siguiente regresa el numero inicial, que en este caso es el numero 11)
+
+  // if the heroes array is not empty, the method below returns the highest
+  //(Si el Arreglo de Herores no esta vacio, el Metodo siguiente regresa el valor mas alto)
+
+  // hero id + 1.
+
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
